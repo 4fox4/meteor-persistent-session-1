bb9148f9777d0a3526c7267f64ba2f3b8b05c93e
Hanafi <hanafi.o95@gmail.com>
2021-10-02 13:09:10 +0200

    Publish atm

--- package.js
+++ package.js
@@ -1,18 +1,26 @@
 Package.describe({
-  name: "cultofcoders:persistent-session",
+  name: "4fox4:persistent-session",
   version: "1.0.0",
   summary: "Persistently store Session data on the client",
-  git: "https://github.com/observaInc/meteor-persistent-session"
+  git: "https://github.com/4fox4/meteor-persistent-session-1",
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('2.4'),
-  api.use(['jquery', 'amplify', 'tracker', 'reactive-dict', 'session', 'underscore', 'ejson']);
+Package.onUse(function (api) {
+  api.versionsFrom("2.4"),
+    api.use([
+      "jquery@1.0.2",
+      "amplify@1.0.0",
+      "tracker",
+      "reactive-dict",
+      "session",
+      "underscore",
+      "ejson",
+    ]);
   // If `accounts-base` is loaded, we have to make sure that this package is
   // loaded after `accounts-base` is, so we specify `weak: true` here
-  api.use('accounts-base', { weak: true });
-  api.addFiles('lib/persistent_session.js', 'client');
-  api.export('PersistentSession', ['client']);
+  api.use("accounts-base", { weak: true });
+  api.addFiles("lib/persistent_session.js", "client");
+  api.export("PersistentSession", ["client"]);
 });
 
 Package.onTest(function (api) {
@@ -21,11 +29,11 @@ Package.onTest(function (api) {
   api.use("random");
   api.use("underscore");
   api.use("reactive-dict"); // we only need this exposed for testing
-  api.use("u2622:persistent-session");
+  api.use("4fox4:persistent-session");
 
   // expose for derping around in console
-  api.export('PersistentSession', ['client']);
-  api.export('ReactiveDict', ['client']);
+  api.export("PersistentSession", ["client"]);
+  api.export("ReactiveDict", ["client"]);
 
   api.addFiles("tests/client/persistent_session.js", "client");
 });
